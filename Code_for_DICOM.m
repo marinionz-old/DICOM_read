9a4be6ae52e44a5c05926e2e3f31983df838aaad
Martin Iniguez <60970451+MarIniOnz@users.noreply.github.com>
2022-09-05 17:16:14 +0200

    Better presentation and comments.

--- Code_for_DICOM.m
+++ Code_for_DICOM.m
@@ -2,91 +2,101 @@
 clc
 clear
 
-folder0 = ''
-directory_name = folder0 + 'HOMEWORK1';
-files=dir('C:\Users\marti\Desktop\2019-2020\2 cuatri\Advanced topics in medical image\Homeworks\Homework1_DICOM\HOMEWORK1');
+% Transform into into a function in the way that the DICOM_READ will have
+% as inputs the original path and the new one.
+original_path='C:\Users\marti\Desktop\2019-2020\2 cuatri\Advanced topics in medical image\Homeworks\Homework1_DICOM\HOMEWORK1\';
+% path_final=input('Please introduce image final path: ','s');
+new_path='C:\Users\marti\Desktop\2019-2020\2 cuatri\Advanced topics in medical image\Homeworks\Homework1_DICOM\';
+
+files = dir([original_path '*.dcm']);
 filename=[];
 len=length(files);
 series_num=0;
 
-if exist([folder0 'TCGA-34-5240'],'dir')
-    rmdir ([folder0 'TCGA-34-5240'],'s');
-end
-
-for i=3:len
+for i=1:len
     
-    filename{i-2}=files(i).name;
-    info=dicominfo([folder0 'HOMEWORK1\' filename{i-2}]);
-    patientID{i-2}=info.PatientID;
-    studyID{i-2}=info.StudyInstanceUID;
-    seriesID{i-2}=info.SeriesInstanceUID;
+    filename{i}=files(i).name;
+    info=dicominfo([original_path  filename{i}]);
     
+    % Get DICOM info
+    patientID{i}=info.PatientID;
+    studyID{i}=info.StudyInstanceUID;
+    seriesID{i}=info.SeriesDescription;
     
     %
-    if ~exist([ folder0 patientID{i-2} ],'dir')
-        mkdir([ folder0 patientID{i-2} ]);
+    if ~exist([ new_path patientID{i} ],'dir')
+        mkdir([ new_path patientID{i} ]);
     end
-    if ~exist([ folder0 patientID{i-2} '\' studyID{i-2} ],'dir')
-        mkdir([ folder0 patientID{i-2} '\' studyID{i-2}]);
+    if ~exist([ new_path patientID{i} '\' studyID{i} ],'dir')
+        mkdir([ new_path patientID{i} '\' studyID{i}]);
     end
-    if ~exist([ folder0 patientID{i-2} '\' studyID{i-2} '\' seriesID{i-2}],'dir')
-        mkdir([ folder0 patientID{i-2} '\' studyID{i-2} '\' seriesID{i-2}]);
+    if ~exist([ new_path patientID{i} '\' studyID{i} '\' seriesID{i}],'dir')
+        mkdir([ new_path patientID{i} '\' studyID{i} '\' seriesID{i}]);
         series_num=series_num+1;
-        series_path{series_num}=[ folder0 patientID{i-2} '\' studyID{i-2} '\' seriesID{i-2}];
+        series_path{series_num}=[ new_path patientID{i} '\' studyID{i} '\' seriesID{i}];
     end
     %
-    copyfile([folder0 'HOMEWORK1\' filename{i-2}],[ folder0 patientID{i-2} '\' studyID{i-2} '\' seriesID{i-2}]);
-    i
+    copyfile([original_path filename{i}],[ new_path patientID{i} '\' studyID{i} '\' seriesID{i}]);
+    
 end
 
 %%
 
-for j=1:series_num
+volume=[];
 
-    images=[];
+for j=1:series_num
+    
+    % Resetting auxiliary variables
     z_pat=[];
     Pat_pos=[];
     img_name=[];
-    sag_view0=[];
     pixmatrix=[];
-    images=dir(series_path{j});
+    
+    % Resetting images used for image acquisition and acquiring them from
+    % each series path.
+    images=[];
+    images=dir([series_path{j} '\*.dcm']);
     len=length(images);
     
-    for p=3:len
-    
+    for p=1:len
+        
         img_info=[];
-        img_name{p-2}=images(p).name;
-        img_info=dicominfo([series_path{j} '\' img_name{p-2}]);
-        Pat_pos{p-2}=img_info.ImagePositionPatient;
-        z_pat(p-2)=Pat_pos{p-2}(3);
-        pixmatrix{p-2}=dicomread(img_info);
+        img_name{p}=images(p).name;
+        img_info=dicominfo([series_path{j} '\' img_name{p}]);
+        Pat_pos{p}=img_info.ImagePositionPatient;
+        
+        z_pat(p)=Pat_pos{p}(3);
+        pixmatrix{p}=dicomread(img_info);
+        pixmatrix{p}=pixmatrix{p}*img_info.RescaleSlope-img_info.RescaleIntercept;
         
     end
-     [order,index]=sort(z_pat,'descend');
-     count0=0;
-     for k=index
-         count0=count0+1;
+    
+    [order,index]=sort(z_pat,'descend');
+    count0=0;
+    
+    for k=index
+        count0=count0+1;
         final_image{j}(:,:,count0)=pixmatrix{k};
-     end
-     
-     figure(j);
-     final_image{j}=final_image{j}*img_info.RescaleSlope;
-     final_image{j}=final_image{j}-img_info.RescaleIntercept;
-     [x,y,z]=size(final_image{j});
-     
-     % Axial
-     imshow(final_image{j}(:,:,floor(z/2)),[min(final_image{j},[],'all') max(final_image{j},[],'all')]);
-     
-     % Sagittal
-     sag_view0=final_image{j}(:,floor(y/2),:);
-     sag_view0=reshape(sag_view0,[x,z]);
-     R2 = makeresampler({'cubic','nearest'},'fill');
-     T0 = maketfom('affine',[0 -2.5 0; 1 0 0; 0 0 0]);
-     sag_view{j} = imtransform(sag_view0,T0,R2);  
-     imshow(sag_view{j},[min(sag_view{j},[],'all') max(sag_view{j},[],'all')]);
-     
-     % Coronal
-     cor_view0=final_image{j}(floor(x/2),:,:);
-     cor_view{j}=reshape(cor_view0,[y z]);
-     imshow(cor_view{j},[min(cor_view{j},[],'all') max(cor_view{j},[],'all')]);
+    end
+    
+    [x,y,z]=size(final_image{j});
+    
+    % 3D volume slices
+    axial{j}=reshape(final_image{j}(:,:,floor(z/2)),x,y);
+    
+    sagit=squeeze(final_image{j}(:,floor(y/2),:));
+    tform1 = affine2d([  0 -img_info.SliceThickness/img_info.PixelSpacing(1); 1 0; 0 0]);
+    sagittal{j}=imwarp(sagit,tform1);
+    sagittal{j}=flipud(sagittal{j});
+    
+    coron=squeeze(final_image{j}(floor(x/2),:,:));
+    tform2 = affine2d([ 0 img_info.SliceThickness/img_info.PixelSpacing(2); -1 0; 0 0]);
+    coronal{j}=imwarp(coron,tform2);
+    coronal{j}=fliplr(coronal{j});
+    
+    matrix_size{j}=[x,y,z];
+    
+    % We will present it in m^3
+    volume(j).size=img_info.PixelSpacing(1)*x*img_info.PixelSpacing(1)*y*img_info.SliceThickness*z/(1000^3);
+    volume(j).series=img_info.SeriesDescription;
 end
